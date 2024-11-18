bdadda18f19592f11c673673e4087edc1ded6104
AliGG95 <bladebeliar95@gmail.com>
2024-11-18 20:43:58 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,18 @@
+bl_info = {
+    "name": "Cycles2Octane Converter",
+    "description": "Convert material nodes from cycles to octane",
+    "author": "Rodrigo Gama",
+    "version": (0, 1, 6),
+    "blender": (3, 0, 1),
+    "location": "View3D",
+    "category": "3D View"}
+
+
+def register():
+    from .addon.register import register_addon
+    register_addon()
+
+
+def unregister():
+    from .addon.register import unregister_addon
+    unregister_addon()
